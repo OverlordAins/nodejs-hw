7d665661f8b044abd0175705579960685ccbaf9d
Daniil <stifflingdagger@gmail.com>
2026-01-12 10:48:47 +0200

    server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-import path from 'node:path';
-import fs from 'node:fs/promises';
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
@@ -7,6 +5,51 @@ import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+// 500 handler
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: 'Internal Server Error',
+    error: err.message,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
