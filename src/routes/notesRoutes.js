dc4b33ad444dbb8fcf68d6f9244e2c8dc6df6789
DaniilVr <stifflingdagger@gmail.com>
2026-01-30 18:44:02 +0200

    shema route fix

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -17,13 +17,13 @@ import { celebrate } from 'celebrate';
 const router = Router();
 
 // router.get('/test-error', causeTestError);
-router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
+router.get('/notes', celebrate({ query: getAllNotesSchema }), getAllNotes);
 router.get(
   '/notes/:noteId',
   celebrate({ params: noteIdParamSchema }),
   getNoteById,
 );
-router.post('/notes', celebrate(createNoteSchema), createNote);
+router.post('/notes', celebrate({ body: createNoteSchema }), createNote);
 router.delete(
   '/notes/:noteId',
   celebrate({ params: noteIdParamSchema }),
