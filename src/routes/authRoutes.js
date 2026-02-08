f086664aa075db28635382df3544e98077ca14a4
DaniilVr <stifflingdagger@gmail.com>
2026-02-08 21:25:20 +0200

    started smtp dev

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,12 +3,14 @@ import { Router } from 'express';
 import {
   registerUserSchema,
   loginUserSchema,
+  requestResetEmailSchema,
 } from '../validations/authValidation.js';
 import {
   registerUser,
   loginUser,
   logoutUser,
   refreshUserSession,
+  requestResetEmail,
 } from '../controllers/authController.js';
 
 const router = Router();
@@ -18,8 +20,15 @@ router.post(
   celebrate({ body: registerUserSchema }),
   registerUser,
 );
+
 router.post('/auth/login', celebrate({ body: loginUserSchema }), loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
+
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
 
 export default router;
